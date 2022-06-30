21b079540f344e8815d70054537344c37acee7b7
agouy <contact@agouy.com>
2022-06-30 15:44:06 +0000

    first version

--- README.md
+++ README.md
@@ -1,50 +1,29 @@
-# Template R operator
-
-The `Template R operator` is a template repository for the creation of R operators in Tercen. An overview of steps for developing an operator are:
-
-1. create a github repo
-2. install tercen_studio
-3. login to tercen_studio
-4. git clone the newly created repo
-5. start developing in R in tercen_studio
-6. add R packages to the repo
-7. push to the github repo
-8. go to tercen and install the operator
-
-More information can be found in the [Tercen developer's guide](https://tercen.github.io/developers_guide/).
-
-Below is the operator README standard structure:
+# Cell Annotation
 
 ##### Description
 
-The `Template R operator` is a template repository for the creation of R operators in Tercen.
+The `cell_annotation_operator` annotates clusters based on a reference table of
+marker vs. cell population correpondence.
 
 ##### Usage
 
 Input projection|.
 ---|---
-`x-axis`        | type, description 
-`y-axis`        | type, description 
-`row`           | type, description 
-`column`        | type, description 
-`colors`        | type, description 
-`labels`        | type, description 
+`y-axis`        | numeric, enrichment score (from the marker_enrichment_operator)
+`row`           | factor, variable (channel, marker) 
+`column`        | factor, group (cluster)
 
 Input parameters|.
 ---|---
-`input_var`        | parameter description
+`annotation_level`        | Annotation level
 
 Output relations|.
 ---|---
-`output_var`        | output relation
-`Operator view`        | view of the Shiny application
-
-##### Details
-
-Details on the computation.
+`population`        | Cell population
+`prob`        | Posterior probability of being part of the population, per column (cluster)
 
 ##### See Also
 
-[template_shiny_operator](https://github.com/tercen/template_shiny_operator)
-, [template_docker_operator](https://github.com/tercen/template_docker_operator)
+[marker_enrichment_operator](https://github.com/tercen/marker_enrichment_operator)
+
 
--- main.R
+++ main.R
@@ -1,11 +1,63 @@
-library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+suppressPackageStartupMessages({
+  library(tercen)
+  library(dplyr, warn.conflicts = FALSE)
+  library(tidyr)
+})
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+if(length(ctx$rnames) != 1) stop("Only one row factor must be projected.")
+
+annotation_level <- ctx$op.value("annotation_level", as.character, "1")
+
+rvals <- ctx$rselect()[[1]]
+
+if(any(grepl("Comp|::", rvals))) {
+  rvals <- unlist(lapply(
+    strsplit(rvals, ":|,| "),
+    tail,
+    1
+  ))
+}
+
+annot <- read.csv("./default_annotation.csv", header = TRUE)
+
+annot <- annot %>% filter(level == annotation_level)
+pos_list <- strsplit(gsub(" ", "", annot$high_markers), "_")
+neg_list <- strsplit(gsub(" ", "", annot$low_markers), "_")
+
+# splitted <- strsplit(annot$markers, "(?<=[+-])", perl = TRUE)
+# pos_list <- lapply(splitted, function(x) gsub("[+]", "", x[grep("[+]", x)]))
+# neg_list <- lapply(splitted, function(x) gsub("[-]", "", x[grep("[-]", x)]))
+
+rvals_in <- rvals[rvals %in% unlist(c(pos_list, neg_list))]
+
+if(length(rvals_in) == 0) stop("No markers found in the annotation. Please check the marker names.")
+msg <- paste0("The following markers have been found in the annotation: ", paste0(rvals_in, collapse = ", "))
+ctx$log(msg)
+
+mat <- ctx$as.matrix()
+rownames(mat) <- rvals
+
+probas <- apply(mat, 2, function(m) {
+  sum_z_pos <- unlist(lapply(pos_list, function (x) { sum(m[x], na.rm = TRUE) }))
+  sum_z_neg <- unlist(lapply(neg_list, function (x) { sum(-m[x], na.rm = TRUE) }))
+  sum_z <- sum_z_pos + sum_z_neg
+  sum_z[abs(sum_z) < 0] <- 0
+  pv <- pnorm(sum_z, lower.tail = TRUE)
+  prob <- round(pv / sum(pv), 4)
+  prob
+})
+
+rownames(probas) <- annot$population
+colnames(probas) <- as.character(1:ncol(probas))
+
+df_out <- probas %>% 
+  as_tibble() %>%
+  mutate(population = rownames(probas)) %>%
+  pivot_longer(names_to = ".ci", values_to = "prob", -population) %>%
+  mutate(.ci = as.integer(.ci) - 1)
+
+df_out %>%
   ctx$addNamespace() %>%
   ctx$save()
