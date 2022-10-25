5f309b3a60696041b682eb69877b81b702c1dc17
lucas501 <lucas.lelann@gmail.com>
2022-10-25 13:21:17 +0000

    add p-value to output

--- main.R
+++ main.R
@@ -12,6 +12,7 @@ ctx = tercenCtx()
 if(length(ctx$rnames) != 1) stop("Only one row factor must be projected.")
 
 #annotation_level <- ctx$op.value("annotation_level", as.character, "1")
+pthres <- ctx$op.value('P-value threshold', as.double, 0.05)
 
 rvals <- ctx$rselect()[[1]]
 
@@ -49,25 +50,66 @@ ctx$log(msg)
 mat <- ctx$as.matrix()
 rownames(mat) <- rvals
 
-probas <- apply(mat, 2, function(m) {
+p_val <- apply(mat, 2, function(m) {
   sum_z_pos <- unlist(lapply(pos_list, function (x) { sum(m[x], na.rm = TRUE) }))
   sum_z_neg <- unlist(lapply(neg_list, function (x) { sum(-m[x], na.rm = TRUE) }))
   sum_z <- sum_z_pos + sum_z_neg
   sum_z[abs(sum_z) < 0] <- 0
-  pv <- pnorm(sum_z, lower.tail = TRUE)
-  prob <- round(pv / sum(pv), 4)
+  pv <- 1-pnorm(sum_z, lower.tail = TRUE)
+  pv
+})
+rownames(p_val) <- annot$population
+colnames(p_val) <- as.character(1:ncol(p_val))
+
+probas <- apply(p_val, 2, function(n) {
+  prob <- round(n / sum(n), 4)
   prob
 })
-
 rownames(probas) <- annot$population
 colnames(probas) <- as.character(1:ncol(probas))
 
-df_out <- probas %>% 
+
+apply(t(p_val), 2, function(x) which.max(x))
+tpv<-t(p_val)
+
+table_pv<-cbind(rownames(tpv),"Unknow")
+for (cluster in rownames(tpv)){
+  
+  res<-which( tpv[cluster,] == min(tpv[cluster,]))
+  if (min(tpv[cluster,])[1]<pthres){
+    table_pv[[as.integer(cluster),2]]<-paste(colnames(tpv)[res], collapse = '_')
+  }
+}
+colnames(table_pv)<-c(".ci","max_population")
+
+####output
+pval_out <- p_val %>% 
+  as_tibble() %>%
+  mutate(population = rownames(p_val)) %>%
+  pivot_longer(names_to = ".ci", values_to = "pv", -population) %>%
+  mutate(.ci = as.integer(.ci) - 1L) %>%
+  ctx$addNamespace() 
+
+prob_out <- probas %>% 
   as_tibble() %>%
   mutate(population = rownames(probas)) %>%
   pivot_longer(names_to = ".ci", values_to = "prob", -population) %>%
   mutate(.ci = as.integer(.ci) - 1L) %>%
-  ctx$addNamespace() 
+  ctx$addNamespace()
+
+tbl_pv_out <- table_pv %>% 
+  as_tibble() %>%
+  mutate(.ci = as.integer(.ci) - 1L) %>%
+  ctx$addNamespace() %>%
+  as_relation() 
+
+df_out<-merge(prob_out,pval_out)
+
+join_res <- df_out %>%
+  left_join_relation(ctx$crelation, ".ci", ctx$crelation$rids) %>%
+  left_join_relation(tbl_pv_out, list(), list()) %>%
+  as_join_operator(ctx$cnames, ctx$cnames)
+
 
 df_out %>%
   ctx$save()
